90838b130ad99999dcab12777a2629402d8f65f2
Andrew Monis <5andrewmonis7@gmail.com>
2022-01-04 15:08:50 -0500

    React code along

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
+import Add from './components/Add'
 
-function App() {
+
+function App(props) {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true, 
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+    },
+
+])
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+
+const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+}
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder:  !task.reminder}: task))
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <Add onAdd={addTask}/>}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onDb={toggleReminder} />
+      : 'No Tasks'}
     </div>
   );
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,11 @@
+import Task from './Task'
+
+const Tasks = (props) => {
+    return (
+        <>
+         {props.tasks.map((task) => (<Task key={task.id} task={task} onDelete={props.onDelete} onDb={props.onDb} />))}
+        </>
+    )
+}
+
+export default Tasks
